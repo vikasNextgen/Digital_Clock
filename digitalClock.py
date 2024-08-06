0afc6114416aac0112807a5b66cd1c027780c7c4
vikas <vikasmocharla72@gmail.com>
2024-08-06 11:03:50 +0530

    changed code accordingly to merge with yellow_screen branch

--- digitalClock.py
+++ digitalClock.py
@@ -45,7 +45,7 @@ canvas.bind('<Configure>', lambda event: create_gradient(canvas, '#000428', '#00
 frame = tk.Frame(canvas, bg='black')
 frame.place(relx=0.5, rely=0.5, anchor='center')
 
-# Stylish heading "vikas"
+# Stylish heading "sai"
 label_heading = tk.Label(frame, text="sai", font=('Helvetica', 36, 'bold'), background='black', foreground='#ffcc00')
 label_heading.pack(anchor='center', pady=20)
 
